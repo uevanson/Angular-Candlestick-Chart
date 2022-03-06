3f5df9a3ff86965ebbef516d1c1b82386f0265cf
uchena.evanson <uchena.evanson@hotmail.com>
2022-03-07 00:52:06 +1300

    more namespace changes

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace Angular_Demo.Pages
+namespace Angular_Candlestick_Chart.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
